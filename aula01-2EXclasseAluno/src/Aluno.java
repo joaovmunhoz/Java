a4bfabeaddfef8e8790d4ec4876aa597b1a06d8d
João Vítor Munhoz <145722461+joaovmunhoz@users.noreply.github.com>
2023-10-06 16:17:13 -0300

    Delete aula01-2EXclasseAluno directory
    organização

--- aula01-2EXclasseAluno/src/Aluno.java
+++
@@ -1,9 +0,0 @@
-public class Aluno {
-    String nome;
-    Integer matricula;
-    int qtdFaltas;
-
-    public void maisFalta() {//FUNÇÃO QUE INCREMENTA A QUANTIDADE DE FALTAS QUANDO FOR CHAMADA
-        qtdFaltas++;
-    }
-}
